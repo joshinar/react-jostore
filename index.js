ec7251c426dbd03483fa45cad5077e39e7997741
joshinar <joshinaren93@gmail.com>
2019-08-18 18:16:59 +0530

    final msg

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
